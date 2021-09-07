4909abf362181cf668259f48ce3e8b9a17580f7e
alice <aliceisjustplaying@gmail.com>
2021-09-07 19:05:32 +0300

    are we done?

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ const iveble = () => {
   const self = "https://iveble.art";
   const randomive = ive[Math.floor(Math.random() * ive.length)];
   const randomable = ble[Math.floor(Math.random() * ble.length)];
-  const output = `${randomive} and ${randomable}`;
+  const output = `${randomive}<br/>and<br/>${randomable}`;
+  const tweetText = `i'm ${randomive} and ${randomable}🥺`;
   document.querySelector("#content").innerHTML = output;
   document.querySelector(".twitter-btn").addEventListener("click", (event) => {
     event.preventDefault();
-    window.open(`https://twitter.com/intent/tweet?text=i%27m%20${output}%20🥺&url=${self}`, "_blank");
+    window.open(`https://twitter.com/intent/tweet?text=${tweetText}&url=${self}`, "_blank");
   });
 };
 
 document.addEventListener("DOMContentLoaded", (event) => {
   iveble();
   document.querySelector("#newplsuwu").addEventListener("click", (event) => {
-    iveble();
     event.preventDefault();
+    e.stopImmediatePropagation();
+    iveble();
   });
 });
